4e4d56d04aa3c31d71156109c3e6961fc9cc8a96
naranburulov <naranburulov@gmail.com>
2022-09-29 16:30:42 -0400

    small fixes at create.html

--- src/main/java/com/cydeo/service/CrudService.java
+++ src/main/java/com/cydeo/service/CrudService.java
@@ -1,6 +1,6 @@
 package com.cydeo.service;
 
-import com.cydeo.dto.UserDTO;
+
 
 import java.util.List;
 
--- src/main/java/com/cydeo/service/UserService.java
+++ src/main/java/com/cydeo/service/UserService.java
@@ -2,17 +2,8 @@ package com.cydeo.service;
 
 import com.cydeo.dto.UserDTO;
 
-import java.util.List;
+
 
 public interface UserService extends CrudService<UserDTO, String>{
-
-    UserDTO save(UserDTO user);
-
-    UserDTO findById(String userName);
-
-    List<UserDTO> findAll();
-
-    void deleteById(String userName);
-
 
 }
