107a2400229d3739f516aaeb74bed5778c45ad95
naranburulov <naranburulov@gmail.com>
2022-10-03 14:32:46 -0400

    user service added to find managers

--- src/main/java/com/cydeo/service/UserService.java
+++ src/main/java/com/cydeo/service/UserService.java
@@ -2,8 +2,10 @@ package com.cydeo.service;
 
 import com.cydeo.dto.UserDTO;
 
+import java.util.List;
 
 
 public interface UserService extends CrudService<UserDTO, String>{
 
+    List<UserDTO> findManagers();
 }
--- src/main/java/com/cydeo/service/impl/UserServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -5,6 +5,7 @@ import com.cydeo.service.UserService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl extends AbstractMapService <UserDTO, String> implements UserService {
@@ -33,5 +34,12 @@ public class UserServiceImpl extends AbstractMapService <UserDTO, String> implem
     @Override
     public void update(UserDTO object) {
         super.update(object.getUserName(), object);
+    }
+
+    @Override
+    public List<UserDTO> findManagers() {
+        return findAll().stream()
+                .filter(user->user.getRole().getId()==2)
+                .collect(Collectors.toList());
     }
 }
