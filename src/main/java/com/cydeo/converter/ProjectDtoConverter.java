06320d001f3d5491763a33844bb799f27f35042e
naranburulov <naranburulov@gmail.com>
2022-10-04 11:37:54 -0400

    TaskConverter added

---
+++ src/main/java/com/cydeo/converter/ProjectDtoConverter.java
@@ -0,0 +1,20 @@
+package com.cydeo.converter;
+
+import com.cydeo.dto.ProjectDTO;
+import com.cydeo.service.ProjectService;
+import org.springframework.core.convert.converter.Converter;
+
+public class ProjectDtoConverter implements Converter<String, ProjectDTO> {
+
+    ProjectService projectService;
+
+    public ProjectDtoConverter(ProjectService projectService) {
+        this.projectService = projectService;
+    }
+
+    @Override
+    public ProjectDTO convert(String source) {
+        return projectService.findById(source);
+    }
+}
+
