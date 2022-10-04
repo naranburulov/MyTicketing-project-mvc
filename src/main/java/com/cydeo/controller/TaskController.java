5a8ccc224385dccbe3a390982d072d34a0a0142b
naranburulov <naranburulov@gmail.com>
2022-10-04 11:46:11 -0400

    delete task created

--- src/main/java/com/cydeo/controller/TaskController.java
+++ src/main/java/com/cydeo/controller/TaskController.java
@@ -7,6 +7,7 @@ import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -44,6 +45,14 @@ public class TaskController {
         return "redirect:/task/create";
     }
 
+    @GetMapping("/delete/{id}")
+    public String deleteTask(@PathVariable("id") Long id){
+
+        taskService.deleteById(id);
+
+
+        return "redirect:/task/create";
+    }
 
 
 }
