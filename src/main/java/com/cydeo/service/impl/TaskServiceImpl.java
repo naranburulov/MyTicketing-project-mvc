042ac434cff6be6a2a6250cd63d45428e151e9c8
naranburulov <naranburulov@gmail.com>
2022-10-04 11:42:42 -0400

    if statements on null and date.now are added to save() - TaskServiceImpl

--- src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
@@ -1,9 +1,11 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.TaskDTO;
+import com.cydeo.enums.Status;
 import com.cydeo.service.TaskService;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -11,6 +13,14 @@ public class TaskServiceImpl extends AbstractMapService<TaskDTO,Long> implements
 
     @Override
     public TaskDTO save(TaskDTO task) {
+
+        if(task.getTaskStatus() ==null){
+            task.setTaskStatus(Status.OPEN);
+        }
+        if (task.getAssignedDate()==null){
+            task.setAssignedDate(LocalDate.now());
+        }
+
         return super.save(task.getId(), task);
     }
 
