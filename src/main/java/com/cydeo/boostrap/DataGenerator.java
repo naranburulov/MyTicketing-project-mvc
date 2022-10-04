bb277437c3be07b3dfcc8422b88e2eb4b71416b5
naranburulov <naranburulov@gmail.com>
2022-10-04 10:44:49 -0400

    task sample data is added to DataGenerator

--- src/main/java/com/cydeo/boostrap/DataGenerator.java
+++ src/main/java/com/cydeo/boostrap/DataGenerator.java
@@ -2,10 +2,12 @@ package com.cydeo.boostrap;
 
 import com.cydeo.dto.ProjectDTO;
 import com.cydeo.dto.RoleDTO;
+import com.cydeo.dto.TaskDTO;
 import com.cydeo.dto.UserDTO;
 import com.cydeo.enums.Gender;
 import com.cydeo.service.ProjectService;
 import com.cydeo.service.RoleService;
+import com.cydeo.service.TaskService;
 import com.cydeo.service.UserService;
 import com.cydeo.enums.Status;
 import org.springframework.boot.CommandLineRunner;
@@ -19,11 +21,13 @@ import java.time.LocalDate;
 public class DataGenerator implements CommandLineRunner {
 
 
+    private final TaskService taskService;
     private final RoleService roleService;
     private final UserService userService;
     private final ProjectService projectService;
 
-    public DataGenerator(RoleService roleService, UserService userService, ProjectService projectService) {
+    public DataGenerator(TaskService taskService, RoleService roleService, UserService userService, ProjectService projectService) {
+        this.taskService = taskService;
         this.roleService = roleService;
         this.userService = userService;
         this.projectService = projectService;
@@ -75,6 +79,17 @@ public class DataGenerator implements CommandLineRunner {
         projectService.save(project2);
         projectService.save(project3);
 
+
+
+        TaskDTO task1 = new TaskDTO(1L, project1, user8, "Controller", "Request Mapping", Status.IN_PROGRESS, LocalDate.now().minusDays(4));
+        TaskDTO task2 = new TaskDTO(2L, project3, user3, "Configuration", "Database Connection", Status.COMPLETE, LocalDate.now().minusDays(12));
+        TaskDTO task3 = new TaskDTO(3L, project3, user6, "Mapping", "One-To-Many", Status.COMPLETE, LocalDate.now().minusDays(8));
+        TaskDTO task4 = new TaskDTO(4L, project2, user7, "Dependency Injection", "Autowired", Status.IN_PROGRESS, LocalDate.now().minusDays(20));
+
+        taskService.save(task1);
+        taskService.save(task2);
+        taskService.save(task3);
+        taskService.save(task4);
 
 
     }
