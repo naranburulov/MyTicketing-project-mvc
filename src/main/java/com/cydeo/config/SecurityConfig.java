e4fc665bf8a128042bab3ac4cd801f9046f9cfb6
naranburulov <naranburulov@gmail.com>
2022-11-24 13:37:19 -0500

    AuthSuccessHandler overriden, SecurityServiceImpl finished, Security Config finished.

--- src/main/java/com/cydeo/config/SecurityConfig.java
+++ src/main/java/com/cydeo/config/SecurityConfig.java
@@ -1,45 +1,22 @@
 package com.cydeo.config;
 
+import com.cydeo.service.SecurityService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 public class SecurityConfig {
 
-    private final SecurityService
+    private final SecurityService securityService;
+    private final AuthSuccessHandler authSuccessHandler;
 
-    //    hard-coded way: OVERRIDES Spring created object :
-//    @Bean         //manually create a User bean that will be used to Authenticate, and give authorization to a user.
-//    public UserDetailsService userDetailsService(PasswordEncoder encoder){
-//
-//              //manually create a list of users  in Security Config and add each user to the list.
-//        List<UserDetails> userList = new ArrayList<>();
-//        userList.add(
-//                //to encode our password when passing the User password
-//                new User("mike", encoder.encode("password"),
-//                        Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN")))
-//        );
-//        userList.add(
-//                new User("ozzy", encoder.encode("password"),
-//                        Arrays.asList(new SimpleGrantedAuthority("ROLE_MANAGER")))
-//        );
-//
-//        return new InMemoryUserDetailsManager(userList);
-//        //This saves to memory rather than DB
-//    }
+    public SecurityConfig(SecurityService securityService, AuthSuccessHandler authSuccessHandler) {
+        this.securityService = securityService;
+        this.authSuccessHandler = authSuccessHandler;
+    }
 
     // introducing my own validation form to Spring (authorization and authentication)
     @Bean
--- src/main/java/com/cydeo/service/impl/SecurityServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/SecurityServiceImpl.java
@@ -1,13 +1,12 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.entity.User;
+import com.cydeo.entity.common.UserPrincipal;
 import com.cydeo.repository.UserRepository;
 import com.cydeo.service.SecurityService;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
-import java.nio.file.attribute.UserPrincipal;
 
 @Service
 public class SecurityServiceImpl implements SecurityService {
@@ -29,8 +28,6 @@ public class SecurityServiceImpl implements SecurityService {
         if (user==null){
             throw new UsernameNotFoundException(username);
         }
-
-
 
         return new UserPrincipal(user);
     }
